1a579b89d7ba1f131103548870408c773a8b8fd2
Prakash <prakashrajbanc1@gmail.com>
2026-06-26 22:17:50 +0545

    Added Checkout page and integrate checkout and create order functionality

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import Home from "./pages/home/Home"
 import Product from "./pages/product/Product"
 import Singleproduct from "./pages/Single-product/SingleProduct"
 import MyCart from "./pages/cart/MyCart"
+import Checkout from "./pages/checkout/Checkout"
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
           <Route path="/products" element={<Product />} />
           <Route path="/products/:id" element={<Singleproduct />} />
           <Route path="/my-cart" element={<MyCart />} />
+          <Route path="/my-checkout" element={<Checkout />} />
         </Routes>
       </BrowserRouter>
     </Provider>
